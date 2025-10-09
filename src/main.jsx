8735e8d3406152321ca68fd7d9d2fd22ca3b530d
Otavio <otaviozanonn@icloud.com>
2025-10-09 15:08:58 -0300

    added missing imports.

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
 import ViewPaste from "./ViewPaste";
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<App />} />
-      <Route path="/:id" element={<ViewPaste />} />
-    </Routes>
-  </BrowserRouter>
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/:id" element={<ViewPaste />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
 );
